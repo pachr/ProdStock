382f5ff4c02a15fbbea96de5a224bf3c6f99fd63
pchretie <paul-alexandre.chretien@ig2i.fr>
2016-06-14 16:41:12 +0200

    Home page design is good

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -23,7 +23,8 @@ public class HomeController extends Controller {
      * <code>GET</code> request with a path of <code>/</code>.
      */
     public Result index() {
-        return ok(index.render("ProdStock Project"));
+        List<Instance> instances = Instance.find.all();
+        return ok(views.html.index.render("ProdStock Project", instances));
     }
 
     public Result script() {
